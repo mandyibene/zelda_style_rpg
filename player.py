16f52771e9319f0e52ebe1d05ac3cc7ca18444bb
Mandy Ibéné <mandy.ibene@gmail.com>
2023-01-28 23:59:18 +0100

    Creating the player

--- player.py
+++ player.py
@@ -3,7 +3,65 @@ from settings import *
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups):
+    def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load('graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+        self.obstacle_sprites = obstacle_sprites
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        elif keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        else:
+            self.direction.x = 0
+
+    def move(self, speed):
+        # normalize magnitude (length of vector) to 1 whatever the direction
+        # so when moving diagonally the player is not faster than orthogonally
+        if self.direction.magnitude() != 0:  # vector of 0 can't be normalized
+            self.direction = self.direction.normalize()
+
+        # self.rect.center += self.direction * speed
+        self.rect.x += self.direction.x * speed
+        self.collision('horizontal')
+        self.rect.y += self.direction.y * speed
+        self.collision('vertical')
+
+    def collision(self, direction):
+        # pygame can tell if there is a collision, but it don't know where
+        # we need to find a way to know where the player and the obstacle are colliding
+
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):  # if rect of sprite collide with rect of player
+                    if self.direction.x > 0:  # moving right
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0:  # moving left
+                        self.rect.left = sprite.rect.right
+
+        if direction == 'vertical':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0:  # moving down
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0:  # moving up
+                        self.rect.top = sprite.rect.bottom
+
+    def update(self):
+        self.input()
+        self.move(self.speed)
