3f74f64316aa6d3e1bf910e21491d95add428833
Mandy Ibéné <mandy.ibene@gmail.com>
2023-01-29 03:02:02 +0100

    Player animations

--- level.py
+++ level.py
@@ -51,7 +51,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        # debug(self.player.direction)
+        debug(self.player.status)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
